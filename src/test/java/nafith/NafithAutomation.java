40046a02ea477915d62b2d4de9ebb003a2017106
sajedakhalayleh <s.khalayleh@Nafithhq.com>
2026-08-16 15:18:45 +0300

    Fix Chrome headless mode for CI

--- src/test/java/nafith/NafithAutomation.java
+++ src/test/java/nafith/NafithAutomation.java
@@ -64,9 +64,10 @@ public class NafithAutomation {
 
         ChromeOptions options = new ChromeOptions();
 
-        options.addArguments("--start-maximized");
+        options.addArguments("--headless=new");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
         options.addArguments("--window-size=1920,1080");
 
         driver = new ChromeDriver(options);
