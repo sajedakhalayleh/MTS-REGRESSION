3f6139286e8e97ca0c143b3e2d219342c2013c79
sajedakhalayleh <s.khalayleh@Nafithhq.com>
2026-08-16 15:38:00 +0300

    Improve parent menu locator

--- src/test/java/nafith/NafithAutomation.java
+++ src/test/java/nafith/NafithAutomation.java
@@ -64,10 +64,9 @@ public class NafithAutomation {
 
         ChromeOptions options = new ChromeOptions();
 
-        options.addArguments("--headless=new");
+        options.addArguments("--start-maximized");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--disable-gpu");
         options.addArguments("--window-size=1920,1080");
 
         driver = new ChromeDriver(options);
